54d6e302c85724ba2a821eb6ae07263ee8acaf29
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-05-10 15:50:22 -0500

    bump version (1.0.0 => 2.0.0), move DialogueTreePatch to a new file, get the actual typespeed from dialoguemanager singleton instead of guessing, reactivate delay

---
+++ DialogueTreePatch.cs
@@ -0,0 +1,67 @@
+﻿using Il2Cpp;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Miside_Zero_Dialogue_Override
+{
+    [HarmonyPatch(typeof(DialogueTree), "PlayNode")]
+    public static class DialogueTreePatch
+    {
+        static void Prefix(DialogueTree __instance, DialogueNode node)
+        {
+            if (node == null)
+            {
+                Mod.Logger.Error("node is null, returning...");
+                return;
+            }
+            try
+            {
+                int index = Mod.MappedNodes.FindIndex(n => n == node);
+
+                if (index == -1)
+                {
+                    Mod.Logger.Error("node does not have an audio clip, returning...");
+                    return;
+                }
+
+                if (Mod.customDtos == null || Mod.customDtos.nodes == null || index >= Mod.customDtos.nodes.Count)
+                {
+                    Mod.Logger.Error($"customDtos missing for index {index}, returning...");
+                    return;
+                }
+
+                DialogueNodeDTO dto = Mod.customDtos.nodes[index];
+                if (dto == null)
+                {
+                    Mod.Logger.Error($"dto at index {index} is null, returning...");
+                    return;
+                }
+
+                string path = NodeAudioManager.GetNodeAudioPath(dto);
+                AudioClip clip = AudioImporter.LoadAudio(path);
+                if (clip == null)
+                {
+                    Mod.Logger.Error("bass.dll audio import failed, returning...");
+                    return;
+                }
+
+                // we're forced to estimate how long it will take based on fps due
+                // to il2cpp making patching coroutines impossible
+
+                // not an ideal fix
+                float typeSpeed = DialogueManager.instance.typeSpeed;
+                float predictedTime = dto.dialogueText.Length * Mathf.Max(typeSpeed, Mod.AvgDt);
+                float fpsCompensation = predictedTime - dto.dialogueText.Length * typeSpeed;
+                float clipLengthCompensation = clip.length - predictedTime;
+                node.dialogueText = dto.dialogueText;
+                node.delay += fpsCompensation + clipLengthCompensation;
+                node.voiceClip = clip;
+            }
+            catch (System.Exception ex)
+            {
+                Mod.Logger.Error($"Failed to patch node with text \"{node.dialogueText}\" " +
+                    $"due to {ex.GetType().Name}: {ex.Message}");
+            }
+        }
+    }
+}
