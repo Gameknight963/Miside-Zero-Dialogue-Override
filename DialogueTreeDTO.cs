f27a3ef119e4187f533d81e79ff8f5e97a1c04a7
Gameknight963 <sebisebpaw@gmail.com>
2026-05-12 17:27:00 -0500

    refactor: migrate to tree-scoped dialogue pack structure

---
+++ DialogueTreeDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MZDO
+{
+    public class DialogueTreeDTO
+    {
+        public List<DialogueNodeDTO> nodes;
+        public List<int> startNodeIds;
+        public float? chirpTime;
+        public float? initialDelay;
+        public float? exitDelay;
+    }
+}
