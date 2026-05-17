768b3cb7cf0ec6e5de2cced9e0ab432be2e7224b
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-05-16 21:14:02 -0500

    Disable pitch randomization for nodes that have audio clip

---
+++ DialogueTreePatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+
+namespace MZDO
+{
+    [HarmonyPatch(typeof(DialogueTree), "PlayNode")]
+    public static class DialogueTreePatch
+    {
+        static void Prefix(DialogueTree __instance, DialogueNode node)
+        {
+            if (node.voiceClip)
+                __instance.GetSpeakerAudioSource(node.speakerName).pitch = 1f;
+        }
+    }
+}
